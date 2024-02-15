d40a94164076eb8c0474808bda4be6d2bca3f808
MayankJaiswal1 <mayankjaiswalofficial1@gmail.com>
2024-02-15 20:22:42 +0530

    Commit

---
+++ ucf2.php
@@ -0,0 +1,400 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Unity Care Foundation
+    </title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/responsive.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+
+<body>
+    <div class="fixed-top">
+
+        <header>
+            <div class="container">
+                <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+                    <a class="navbar-brand" href="#home">Unity Care Foundation</a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse"
+                        data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                        aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav ml-auto">
+                            <li class="nav-item active">
+                                <a class="nav-link" href="#home">Home <span class="sr-only">(current)</span></a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#donation">Donations</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#mission-id">Missions</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#about">About</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#contact">Contact</a>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
+        </header>
+
+        <div class="cont-sec">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-6">
+                        <p>Contact No : <a href="tel: +9198659****59">+91 98659****59</a></p>
+                    </div>
+                    <div class="col-lg-6">
+                        <div class="social">
+                            <a href="https://www.facebook.com/"><img src="img/icons/facebook.png" alt="facebook"></a>
+                            <a href="https://www.instagram.com/"><img src="img/icons/instagram.png" alt="inatagram"></a>
+                            <a href="https://www.youtube.com/"><img src="img/icons/youtube.png" alt="youtube"></a>
+                            <a href="https://in.linkedin.com/"><img src="img/icons/linkedin.png" alt="linkedin"></a>
+                            <a href="https://mail.google.com/mail/u/0/#inbox"><img src="img/icons/gmail.png" alt="gnail"></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+    <section class="home-sec" id="home">
+        <div class="container">
+            <div class="home-content">
+                <div class="row">
+                    <div class="col-lg-6 align-item-center">
+                        <div class="home-info">
+                            <h1>Alone we can do little, together we can do so much</h1>
+                            <h2>We <span>Unity care foundation</span> manage for needy peoples</h2>
+                            <p>we believe in the power of compassion and community to make a positive impact on the lives of those in need. Our mission is to extend a helping hand to the less fortunate, providing essential support and resources to create a brighter future for all.</p>
+                            <div class="buttons">
+                                <a href="#contact" class="btn1">Donate now</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 order-first order-lg-last">
+                        <div class="img-sec">
+                            <img src="img/img-1.png" alt="home-image">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="don-sec" id="donation">
+        <div class="container">
+            <div class="heading">
+                <h2>We Manage Donation like..</h2>
+            </div>
+            <div class="row">
+                <div class="col-lg-4">
+                    <div class="don-box">
+                        <img src="img/don/clothing.png" alt="img">
+                        <h3>Clothes</h3>
+                        <p>Wrap someone in warmth, donate your old charm!</p>
+                        <a href="#contact" class="btn1">Donate Now</a>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="don-box">
+                        <img src="img/don/sneakers.png" alt="img">
+                        <h3>Footware</h3>
+                        <p>Step into kindness, donate footwear for a brighter journey!</p>
+                        <a href="#contact" class="btn1">Donate Now</a>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="don-box">
+                        <img src="img/don/salary.png" alt="img">
+                        <h3>Fund</h3>
+                        <p>Fuel hope, fund change-every contribution counts!</p>
+                        <a href="#contact" class="btn1">Donate Now</a>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="don-box">
+                        <img src="img/don/gadgets.png" alt="img">
+                        <h3>Gadgets</h3>
+                        <p>Empower with Tech, Donate Gadgets-Bridging the Digital Divide!</p>
+                        <a href="#contact" class="btn1">Donate Now</a>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="don-box">
+                        <img src="img/don/book.png" alt="img">
+                        <h3>Stationary</h3>
+                        <p>Empower Minds, Donate Stationery-Write the Future Together!</p>
+                        <a href="#contact" class="btn1">Donate Now</a>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="don-box">
+                        <img src="img/don/shopping-bag.png" alt="img">
+                        <h3>Food</h3>
+                        <p>Nourish the Soul, Donate Food-Sustaining Lives, One Meal at a Time!</p>
+                        <a href="#contact" class="btn1">Donate Now</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="mission" id="mission-id">
+        <div class="container">
+            <div class="heading">
+                <h2>Our Missions</h2>
+                <p>We have delivered <span>Wastage</span> or <span>Donations</span> to needy Peoples</p>
+            </div>
+            <div class="gallery-sec">
+                <div class="container">
+                    <div class="image-container">
+                        <div class="image"><img src="img/miss/1.jpg" alt="img"></div>
+                        <div class="image"><img src="img/miss/2.jpg" alt="img"></div>
+                        <div class="image"><img src="img/miss/3.jpg" alt="img"></div>
+                        <div class="image"><img src="img/miss/4.jpg" alt="img"></div>
+                        <div class="image"><img src="img/miss/5.jpg" alt="img"></div>
+                        <div class="image"><img src="img/miss/6.jpg" alt="img"></div>
+                    </div>
+                </div>
+                <div class="pop-image">
+                    <span>&times;</span>
+                    <img src="img/gallery/1.jpg" alt="gallery-img">
+                </div>
+                </di v>
+            </div>
+    </section>
+
+    <section class="about-sec" id="about">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4 about-img">
+                    <img src="img/img-2.jpeg" alt="about">
+                </div>
+                <div class="col-lg-8 order-first order-lg-last">
+                    <div class="heading">
+                        <h2>What We Do & Why We Do</h2>
+                    </div>
+                    <p>At Unity Cares Foundation, we're on a mission to make a positive impact on the lives of those in need. We provide essential resources like food, shelter, education, and healthcare because we believe in addressing challenges directly. We do this because we understand that everyone deserves support during tough times. </p>
+                    <p>By joining us, you become part of a community that actively works to break the cycle of poverty, create opportunities, and empower individuals. Together, we're building a world where kindness and collective action lead to meaningful and lasting change.
+
+
+
+
+
+                    </p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+        <!-- Contact Section -->
+        <!-- <section class="contact-section" id="contact">
+            <div class="container">
+                <div class="heading">
+                    <h2>Connect With Us</h2>
+                    <p>Fill this form, our team will collect your <span>Donation</span> or <span>Wastage</span> from your place.</p>
+                </div>
+                <div class="row">
+                    <div class="col-lg-12 mt-5">
+                        <form class="contact-form">
+                            <div class="row">
+                                <div class="col-lg-12">
+                                    <div class="row">
+                                        <div class="col-lg-6">
+                                            <div class="col-lg-12">
+                                                <div class="form-group">
+                                                    <input type="text" class="form-control" placeholder="Your Name">
+                                                </div>
+                                            </div>
+                                            <div class="col-lg-12">
+                                                <div class="form-group">
+                                                    <input type="text" class="form-control" placeholder="Mobile No.">
+                                                </div>
+                                            </div>
+                                            <div class="col-lg-12">
+                                                <div class="form-group">
+                                                    <select name="donation" id="donation">
+                                                        <option value="">Choose Donation or Wastage</option>
+                                                        <option value="food">Food</option>
+                                                        <option value="clothes">Clothes</option>
+                                                        <option value="footware">Footware</option>
+                                                        <option value="books">Books</option>
+                                                        <option value="fund">Fund</option>
+                                                        <option value="gadget">Ele. Gadgets</option>
+                                                    </select>
+                                                </div>
+                                            </div>
+                                            
+                                        </div>
+                                        <div class="col-lg-6">
+                                            <div class="form-group">
+                                                <input type="text" class="form-control" placeholder="Address">
+                                            </div>
+                                            <div class="form-group">
+                                                <textarea class="form-control" name="text" id="" cols="90" rows="1" placeholder="Message"></textarea>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-12 mt-3">
+                                            <a href="#" class="btn1 mt-5">Submit Details</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </section> -->
+
+        <?php
+        if(isset($_POST['s1'])){
+            $name=$_POST['a1'];//Extract data
+            $cnum=$_POST['a2'];
+            $sel=$_POST['a3'];
+            $don=$_POST['a4'];
+            $mess=$_POST['a5'];
+            $conn=mysqli_connect("localhost","root","","stud_database");
+        $query= "insert into ucf values('$name','$cnum','$sel','$don','$mess')";
+        mysqli_query($conn,$query);
+            // echo("You click Button");
+        ?>
+        <script>
+            alert("Registration Done Successfully");
+        </script>    
+        <?php
+        }
+        ?>
+
+        <div class="card">
+            <div class="cardbody">
+                <form action="ucf2.php" method="POST" >
+        
+                    <div class="heading">
+                        <h2>Connect With Us</h2>
+                        <p>Fill this form, our team will collect your <span>Donation</span> or <span>Wastage</span> from your place.</p>
+                    </div>
+                    <div class="input">
+                           Name    <input type="text"  name='a1'>
+                    </div>
+                        <br/>
+                    <div class="input">
+                        Mobile  <input type="text" name='a2' >
+                    <br/>
+                    </div>
+                    <br>
+                    <div class="menu">
+                        <select name="a3" id=""> 
+                            <option value="">Choose Donation </option>
+                            <option value="Food"  name="a3">Food </option>
+                            <option value="Clothes"  name="a3">Clothes</option>
+                            <option value="Footware"  name="a3">Footware </option>
+                            <option value="Books"  name="a3">Books </option>
+                            <option value="Toys"  name="a3">Toys </option>
+                            <option value="Funds"  name="a3">Funds </option>
+                        </select>
+                    </div>
+                    <br/>
+                    <div class="input">
+                        Donation:<input type="text"  name='a4'>
+                    </div>
+                    <br/>
+                    <div>
+                        Message:<input type="text"  name='a5'>
+                    </div>
+                    <br/>
+                    <div class="btn2">
+                       <input type="submit" name='s1'>
+                    </div>
+                
+                </form>    
+            
+             
+            </div>
+        </div>
+
+    <!-- <form action="ucf1.php" method="POST" >
+        
+        Name<input type="text"  name='a1'>
+        <br/>
+        Mobile No.<input type="text" name='a2' >
+        <br/>
+        
+        <select name="a3" id=""> 
+            <option value="Food"  name="a3">Food </option>
+            <option value="Clothes"  name="a3">Clothes</option>
+            <option value="Footware"  name="a3">Footware </option>
+            <option value="Books"  name="a3">Books </option>
+            <option value="Toys"  name="a3">Toys </option>
+            <option value="Funds"  name="a3">Funds </option>
+        </select>
+        <br/>
+        Donation Type:<input type="text"  name='a4'>
+        <br/>
+
+        Message:<input type="text"  name='a5'>
+        <br/>
+        <input type="submit" name='s1'>
+    </form>    
+
+  -->
+
+    <footer>
+        <div class="container">
+            <div class="row">
+
+                <div class="col-lg-4">
+                    <div class="col-one">
+                        <h4>Code Convent foundation</h4>
+                        <p>Address : 3, Near IT Park, Mahatma Gandhi Road, Mumbai 440000 </p>
+                        <p>Contact No : <a href="tel: +91 98659****59">+91 98659****59</a></p>
+                        <p>Email : <a href="mailto:foundation@code.com">foundation@code.com</a></p>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="col-two">
+                        <h4>Important Links</h4>
+                        <ul>
+                            <li><a href="#home">Home</a></li>
+                            <li><a href="#donation">Donations</a></li>
+                            <li><a href="#mission-id">Missions</a></li>
+                            <li><a href="#about">About us</a></li>
+                            <li><a href="#contact">Contact us</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="col-one">
+                        <h4>Social Media</h4>
+                        <div class="social">
+                            <a href="#"><img src="img/icons/facebook.png" alt="facebook"></a>
+                            <a href="#"><img src="img/icons/instagram.png" alt="inatagram"></a>
+                            <a href="#"><img src="img/icons/youtube.png" alt="youtube"></a>
+                            <a href="#"><img src="img/icons/linkedin.png" alt="linkedin"></a>
+                            <a href="#"><img src="img/icons/gmail.png" alt="gnail"></a>
+                        </div>
+                        <p>Copyright &copy; 2022 | All Right Reserved</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <script src="js/script.js"></script>
+</body>
+
+</html>
